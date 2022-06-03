b97fefc4b5d6874f42297931367a6daac681236f
bloombar <amos@cs.nyu.edu>
2022-06-02 21:36:43 -0400

    jekyll experiment

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  overrides: [
+    {
+      files: "docs/**/*.html",
+      options: { parser: "markdown" },
+    },
+  ],
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  arrowParens: "avoid",
+}
